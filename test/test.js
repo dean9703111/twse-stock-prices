b3c3454568b2d3e2bf30bf7901b333e40d690db6
dean_lin <dean_lin@ubiqconn.com>
2020-05-05 17:17:51 +0800

    update query logic

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var twseStockPrices = require('../twse-stock-prices');
 
-twseStockPrices.getCurrentPrice(['1101'], function(err, price){
+twseStockPrices.getCurrentPrice(['3029', '2408', '8437', '1256', '8462'], function(err, price){
 	console.log(price);
 });
