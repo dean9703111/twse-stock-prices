1ef33699a89f60768031d3f080d9856fb3a8fe5e
dean_lin <dean_lin@ubiqconn.com>
2020-05-06 08:56:08 +0800

    query tse & toc

--- twse-stock-prices.js
+++ twse-stock-prices.js
@@ -4,8 +4,11 @@ const baseUrl = 'http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=';
 
 module.exports.getCurrentPrice = function (stocks, callback) {
 
+	let allStocks = [];
+	// 先抓取上市股票&上櫃股票
 	var query = ''
-	stocks.forEach(stock => query = query + 'tse_' + stock + '.tw|');
+	//改為一次抓兩種，必經如果錯誤並不會回傳資訊
+	stocks.forEach(stock => query = query + 'tse_' + stock + '.tw|' + 'otc_' + stock + '.tw|');
 	query = query.substring(0, query.lastIndexOf('|'));
 	query = query + '&json=1&delay=0&_=' + Date.now();;
 	// console.log(baseUrl + query);
